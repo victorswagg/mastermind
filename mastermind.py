92661d78696874d3ae4ce8c4f4673759c0bc5094
Guillaume <guillaume.janssens@epitech.eu>
2020-12-07 19:07:15 +0100

    fix py file + add json file

--- mastermind.py
+++ mastermind.py
@@ -6,75 +6,26 @@
 import sys
 import math
 import random
+import json
+
+def save_score(pseudo, score):
+	with open ("score.json") as file:
+		json_data = json.load(file)
+	with open ("score.json","w") as file:
+		json_data["previous"].append({pseud:score})
+		json.dump(json_data, file)
 
 def mega():
     tries = 0;
-    colors = [random.randint(1, 8), random.randint(1, 8), random.randint(1, 8), random.randint(1, 8), random.randint(1, 8)]
-################
-    for i in range(len(colors)):
-        if colors[i] == 1:
-            colors[i] = "red"
-        elif colors[i] == 2:
-            colors[i] = "blue"
-        elif colors[i] == 3:
-            colors[i] = "yellow"
-        elif colors[i] == 4:
-            colors[i] = "green"
-        elif colors[i] == 5:
-            colors[i] = "black"
-        elif colors[i] == 6:
-            colors[i] = "white"
-        elif colors[i] == 7:
-            colors[i] = "orange"
-        elif colors[i] == 8:
-            colors[i] = "pink"
-        print(i)
-##################
-        print (colors)
+    all_colors = ["red", "blue","yellow","green","black","white","orange","pink"]
+    colors = []
 
-        while tries < 12 and tries != 20 :
-            guess = input("enter your guess\n")
-            guess = guess.split(" ")
-    
-            if len(guess) == 4 and guess == colors:
-                print("congrats, good answer\n")
-                score = 10 - tries
-                print("your score is:\t", score)
-                tries = 20
-            else:
-                print("wrong answer, try again")
-                tries += 1
-                
-        if tries == 12:
-            print("FAILURE, you've used all your attempts")
-            score = 0
-        pseudo = input("save your score by entering your pseudo\n")
+    for i in range(0,5):
+       colors.append(random.choice(all_colors))
 
-def normal():
-    tries = 0;
-    i = 3;
-    colors = [random.randint(1, 6), random.randint(1, 6), random.randint(1, 6), random.randint(1, 6)]
-################
-    while i >= 0:
-        if colors[i] == 1:
-            colors[i] = "rouge"
-        elif colors[i] == 2:
-            colors[i] = "bleu"
-        elif colors[i] == 3:
-            colors[i] = "jaune"
-        elif colors[i] == 4:
-            colors[i] = "vert"
-        elif colors[i] == 5:
-            colors[i] = "noir"
-        elif colors[i] == 6:
-            colors[i] = "blanc"
-        print(i)
-        i -= 1
-##################
-        print (colors)
-
-        while tries < 10 and tries != 20 :
-            guess = input("enter your guess\n")
+    print(colors)
+    while tries < 12 and tries != 20 :
+        guess = input("enter your guess\n")
         guess = guess.split(" ")
     
         if len(guess) == 4 and guess == colors:
@@ -86,10 +37,40 @@ def normal():
             print("wrong answer, try again")
             tries += 1
             
-        if tries == 10:
-                print("FAILURE, you've used all your attempts")
-                score = 0
-        return(score)
+    if tries == 12:
+        print("FAILURE, you've used all your attempts")
+        score = 0
+    return score
+
+
+def normal():
+    tries = 0;
+    i = 3;
+    all_colors = ["red", "blue","yellow","green","black","white"]
+    colors = []
+
+    for i in range(0,4):
+       colors.append(random.choice(all_colors))
+
+    print (colors)
+
+    while tries < 10 and tries != 20 :
+        guess = input("enter your guess\n")
+    guess = guess.split(" ")
+    
+    if len(guess) == 4 and guess == colors:
+        print("congrats, good answer\n")
+        score = 10 - tries
+        print("your score is:\t", score)
+        tries = 20
+    else:
+        print("wrong answer, try again")
+        tries += 1
+        
+    if tries == 10:
+            print("FAILURE, you've used all your attempts")
+            score = 0
+    return(score)
 
 
 
